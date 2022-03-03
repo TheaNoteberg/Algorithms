552868dfb4196a4353e2dd22d85333db7d304cb7
TheaNoteberg <thea.noteberg@gmail.com>
2022-03-03 14:25:07 +0100

    fix: remove crossproduct test from PlaneTest

--- src/test/java/com/williamfiset/algorithms/geometry/PlaneTest.java
+++ src/test/java/com/williamfiset/algorithms/geometry/PlaneTest.java
@@ -3,21 +3,6 @@ import static com.google.common.truth.Truth.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.*;
 public class PlaneTest {
-    @Test
-        public void testCrossProduct(){
-            //Vector vector1 = new Vector(1, 2,3);
-            //Vector vector2 = new Vector(1,2,3);
-            //Vector expectedOutput = new Vector(1, 1, 1);
-            double[] vector1 = {1, 2, 3};
-            double[] vector2 = {3, 3, 3};
-            double[] expectedOutput = {-3, 6, -3};
-            Plane plane = new Plane(1.0, 1.0, 1.0, 1.0);
-            double[] result = plane.crossProduct(vector1, vector2);
-            for (int i = 0; i< expectedOutput.length; i++) {
-                assertEquals(expectedOutput[i], result[i]);
-            }
-
-        }
 
     @Test
         public void testGetNormalVector(){
@@ -30,15 +15,6 @@ public class PlaneTest {
             }
         }
 
-    //Input two 4D vectors
-    //Expected output exception
-    @Test(expected = ArithmeticException.class)
-    public void planeException(){
-        double[] vector1 = {1, 2, 3,4};
-        double[] vector2 = {3, 3, 3, 3};
-        Plane plane = new Plane(1.0, 1.0, 1.0, 1.0);
-        double[] result = plane.crossProduct(vector1, vector2);
 
-    }
 
 }
